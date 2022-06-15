630c71b8d2adb0712c8dc9d09520de4bffeaa2a7
Ken Sodemann <ken@ionic.io>
2022-06-15 10:34:15 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hide-the-page',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
